91349bd4d36f6e97d6c2a766a44f920010fee80a
James <jrm.general@gmail.com>
2013-02-25 13:27:43 -0500

    cleaned layout.html base template

--- js/app/router.js
+++ js/app/router.js
@@ -22,12 +22,6 @@ define([
   var initialize = function(options){
     var appView = options.appView;
     var router = new AppRouter(options);
-    router.on('route:optimize', function () {
-      require(['views/optimize/page'], function (OptimizePage) {
-        var optimizePage = Vm.create(appView, 'OptimizePage', OptimizePage);
-        optimizePage.render();
-      });
-    });
     router.on('route:defaultAction', function (actions) {
       require(['views/base/page'], function (DashboardPage) {
         var dashboardPage = Vm.create(appView, 'DashboardPage', DashboardPage);
@@ -38,18 +32,6 @@ define([
      require(['views/modules/page'], function (ModulePage) {
         var modulePage = Vm.create(appView, 'ModulesPage', ModulePage);
         modulePage.render();
-      });
-    });
-    router.on('route:backbone', function (section) {
-      require(['views/backbone/page'], function (BackbonePage) {
-        var backbonePage = Vm.create(appView, 'BackbonePage', BackbonePage, {section: section});
-        backbonePage.render();
-      });
-    });
-    router.on('route:manager', function () {
-      require(['views/manager/page'], function (ManagerPage) {
-        var managerPage = Vm.create(appView, 'ManagerPage', ManagerPage);
-        managerPage.render();
       });
     });
     Backbone.history.start();
